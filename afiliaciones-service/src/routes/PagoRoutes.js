47db5e73d5162e0dc750491e48bb3f23fcfedb24
LizethDanielaV <lizethdanielavb@ufps.edu.co>
2025-11-06 11:36:00 -0500

    servicio registrar referido

--- afiliaciones-service/src/routes/PagoRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import PagoController from "../controllers/PagoController.js";
-
-const router = Router();
-
-router.post("/", PagoController.registrar);
-
-export default router;
--- notificaciones-service/src/routes/notificaciones.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { enviarCorreoController } from "../controllers/NotificacionController.js";
-
-const router = Router();
-
-router.post("/email", enviarCorreoController);
-
-export default router;
